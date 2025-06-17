c86d7fc056dc6d77cd196adb791237718cc22040
Volod1aPV <panovikvoloda1@gmail.com>
2025-06-17 16:09:52 +0200

    Castling

---
+++ šachy-upg/Moves/Castle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace šachy_upg
+{
+    class Castle : Move
+    {
+        public override MoveType Type { get; }
+        public override Pozice FromPos { get; }
+        public override Pozice ToPos { get; }
+
+        private readonly Smer kingMoveDir;
+        private readonly Pozice rookFromPos;
+        private readonly Pozice rookToPos;
+
+        public Castle(MoveType type, Pozice kingPos)
+        {
+            Type = type;
+            FromPos = kingPos;
+
+            if (type == MoveType.CastleKS)
+            {
+                kingMoveDir = Smer.East;
+                ToPos = new Pozice(kingPos.Row, 6);
+                rookFromPos = new Pozice(kingPos.Row, 7);
+                rookToPos = new Pozice(kingPos.Row, 5);
+            }
+            else if (type == MoveType.CastleQS) 
+            {
+                kingMoveDir = Smer.West;
+                ToPos = new Pozice(kingPos.Row, 2);
+                rookFromPos = new Pozice(kingPos.Row, 0);
+                rookFromPos = new Pozice(kingPos.Row,3);
+            }
+        }
+
+        public override void Execute(Deska deska)
+        {
+            new NormalMove(FromPos, ToPos).Execute(deska);
+            new NormalMove(rookFromPos, rookToPos).Execute(deska);
+
+        }
+
+        public override bool IsLegal(Deska deska)
+        {
+            hrac hrac = deska[FromPos].Color;
+
+            if (deska.IsIncheck(hrac))
+            {
+                return false;
+            }
+
+            Deska copy = deska.Copy();
+            Pozice kingPosInCopy = FromPos;
+
+            for (int i = 0; i < 2; i++)
+            {
+                new NormalMove(kingPosInCopy, kingPosInCopy + kingMoveDir).Execute(copy);
+                kingPosInCopy += kingMoveDir;
+
+                if (copy.IsIncheck(hrac) )
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- šachy-upg/Pieces/King.cs
+++ šachy-upg/Pieces/King.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace šachy_upg
+﻿namespace šachy_upg
 {
     public class King : Piece
     {
@@ -28,6 +22,47 @@ namespace šachy_upg
             Color = color;
         }
 
+        private static bool IsUnmovedRook(Pozice pos, Deska deska)
+        {
+            if (deska.IsEmpty(pos))
+            {
+                return false;
+            }
+
+            Piece piece = deska[pos];
+            return piece.Type == PieceType.Rook && !piece.HasMowed;
+        }
+
+        private static bool AllEmpty(IEnumerable<Pozice> pozices, Deska deska) 
+        {
+            return pozices.All(pos => deska.IsEmpty(pos));
+        }
+
+        private bool CanCastleKingSide(Pozice from, Deska deska)
+        {
+            if (HasMowed)
+            {
+                return false;
+            }
+
+            Pozice rookPos = new Pozice(from.Row, 7);
+            Pozice[] betweenPositions = new Pozice[] { new(from.Row, 5), new(from.Row, 6) };
+
+            return IsUnmovedRook(rookPos, deska) && AllEmpty(betweenPositions, deska);
+        }
+
+        private bool CanCastleQueenSide(Pozice from, Deska deska)
+        {
+            if (HasMowed)
+            {
+                return false;
+            }
+
+            Pozice rookPos = new Pozice(from.Row, 0);
+            Pozice[] betweenPositions = new Pozice[] { new(from.Row, 1), new(from.Row, 2), new(from.Row, 3) };
+
+            return IsUnmovedRook(rookPos, deska) && AllEmpty(betweenPositions, deska);
+        }
         public override Piece Copy()
         {
             King copy = new King(Color);
@@ -60,6 +95,16 @@ namespace šachy_upg
             foreach (Pozice to in MovePositions(from, deska))
             {
                 yield return new NormalMove(from, to);
+            }
+
+            if (CanCastleKingSide(from, deska))
+            {
+                yield return new Castle(MoveType.CastleKS, from);
+            }
+
+            if (CanCastleQueenSide(from, deska))
+            {
+                yield return new Castle(MoveType.CastleQS, from);
             }
         }
 
